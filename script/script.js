7088c0cb3a49adfba84681c010ce84e005c5e825
Agiothyella <agiothyella@gmail.com>
2021-07-23 17:59:02 +0700

    Finished sections
    + Header
    + Services
    + Influence

--- script/script.js
+++ script/script.js
@@ -1,1 +1,22 @@
 `use strict`;
+
+const slideImg = document.getElementsByClassName("slider__img");
+let indexImg = Math.floor(Math.random() * slideImg.length) + 1;
+
+function showSlidesHead() {
+  for (i = 0; i < slideImg.length; i++) {
+    slideImg[i].classList.remove("u-visible");
+  }
+
+  indexImg++;
+
+  if (indexImg > slideImg.length) {
+    indexImg = 1;
+  }
+
+  slideImg[indexImg - 1].classList.add("u-visible");
+
+  timerHead = setTimeout(showSlidesHead, 8000);
+}
+
+showSlidesHead();
