fb868ad07e25bb8b46865d4228e382d7d90260fa
Agiothyella <agiothyella@gmail.com>
2021-07-25 17:01:10 +0700

    Nav & Footer
    + Nav
    + Footer
    + Scroll function

--- script/script.js
+++ script/script.js
@@ -1,5 +1,23 @@
 `use strict`;
 
+const nav = document.querySelector(".nav");
+const navLogo = document.querySelector(".nav__logo");
+
+function showNav() {
+  let top = window.pageYOffset || document.documentElement.scrollTop;
+  if (top > 1) {
+    nav.classList.add("nav-active");
+    navLogo.classList.add("u-visible");
+  } else {
+    nav.classList.remove("nav-active");
+    navLogo.classList.remove("u-visible");
+  }
+}
+
+window.onscroll = function () {
+  showNav();
+};
+
 const slideImg = document.getElementsByClassName("slider__img");
 let indexImg = Math.floor(Math.random() * slideImg.length) + 1;
 
